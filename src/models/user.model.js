4ccf8da9f4edf9209c24855a951b424c20d2782b
Prince Kumar <neo11prince@gmail.com>
2024-01-07 15:35:28 +0530

    modifications

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,27 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+const crypto = require('crypto');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const { toJSON, paginate } = require('./plugins');
 const { roles } = require('../config/roles');
 
-const userSchema = mongoose.Schema(
+const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, 'Please enter your full name'],
+      min: [2, 'Name is too short!'],
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: [true, 'Please enter a username'],
+      min: [2, 'Name is too short!'],
       trim: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Please enter a valid email address'],
       unique: true,
       trim: true,
       lowercase: true,
@@ -40,6 +48,33 @@ const userSchema = mongoose.Schema(
       enum: roles,
       default: 'user',
     },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    profile: {
+      type: Schema.Types.ObjectId,
+      ref: 'Profile',
+    },
+    starred: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Profile',
+      },
+    ],
+    watching: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Profile',
+      },
+    ],
+    created_at: {
+      type: Date,
+      default: Date.now(),
+    },
     isEmailVerified: {
       type: Boolean,
       default: false,
@@ -47,12 +82,14 @@ const userSchema = mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 // add plugin that converts mongoose to json
 userSchema.plugin(toJSON);
 userSchema.plugin(paginate);
+
+userSchema.index({ name: 1, username: 1, email: 1 });
 
 /**
  * Check if email is taken
@@ -63,6 +100,10 @@ userSchema.plugin(paginate);
 userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
   const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
   return !!user;
+};
+
+userSchema.statics.encryptPasswordResetToken = function (token) {
+  return crypto.createHash('sha256').update(token).digest('hex');
 };
 
 /**
@@ -78,14 +119,42 @@ userSchema.methods.isPasswordMatch = async function (password) {
 userSchema.pre('save', async function (next) {
   const user = this;
   if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 8);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
   }
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.methods.changedPasswordSinceJWT = function (JWTTimestamp) {
+  let hasPasswordChanged = false;
+
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    hasPasswordChanged = JWTTimestamp < changedTimestamp;
+  }
+  return hasPasswordChanged;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = this.constructor.encryptPasswordResetToken(resetToken);
+
+  this.passwordResetExpires = Date.now() + process.env.PASSWORD_RESET_EXPIRY_MINUTES * 60 * 1000;
+
+  return resetToken;
+};
+
 /**
  * @typedef User
  */
-const User = mongoose.model('User', userSchema);
+const User = model('User', userSchema, 'users');
 
 module.exports = User;
