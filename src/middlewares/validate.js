c52caab14dff03174eb60eaaef70387173a2659a
Prince Kumar <neo11prince@gmail.com>
2024-01-08 10:40:09 +0530

    added express validator

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const httpStatus = require('http-status');
+const { validationResult } = require('express-validator');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 
@@ -18,4 +19,11 @@ const validate = (schema) => (req, res, next) => {
   return next();
 };
 
-module.exports = validate;
+const validateWithExpressValidaor = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) return next();
+  errors.name = 'ExpressValidationErrors';
+  next(errors);
+};
+
+module.exports = { validate, validateWithExpressValidaor };
--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const validate = require('../../middlewares/validate');
+const { validate } = require('../../middlewares/validate');
 const authValidation = require('../../validations/auth.validation');
 const authController = require('../../controllers/auth.controller');
 const auth = require('../../middlewares/auth');
